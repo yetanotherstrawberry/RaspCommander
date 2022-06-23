683696f44ae2bf885fb59ace4ccfe6164c5d0161
yetanotherstrawberry <106765930+yetanotherstrawberry@users.noreply.github.com>
2022-06-24 00:21:02 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace RaspCommander
+{
+
+    public partial class App : Application
+    {
+
+    }
+
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+﻿Klawisze:
+L - wyświetl licencję
+F7 - nowy folder
+F8 - usuń
